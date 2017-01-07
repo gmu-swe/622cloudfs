3fc0e4b5f0655af47368dddf0b151e11147aeb39
Jonathan Bell <jbell@cs.columbia.edu>
2017-01-06 19:50:17 -0500

    Add lock

--- fusedriver/src/main/java/edu/gmu/swe622/struct/CloudPath.java
+++ fusedriver/src/main/java/edu/gmu/swe622/struct/CloudPath.java
@@ -1,33 +1,37 @@
 package edu.gmu.swe622.struct;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import edu.gmu.swe622.struct.internal.Poolable;
 
 /**
  * Representation of a file or directory that's stored in our FS
  * 
- * Don't mess with poolIdx - it's used internally by the filesystem bridge. 
- * Use the other constructors.
+ * Don't mess with poolIdx - it's used internally by the filesystem bridge. Use
+ * the other constructors.
  *
  */
 public abstract class CloudPath implements Poolable {
 	private final int poolIdx;
 
 	/**
-	 * Hint: You might want to use the "parent" in your memory DB implementation :)
+	 * Hint: You might want to use the "parent" in your memory DB implementation
+	 * :)
 	 */
 	protected CloudDirectory parent;
 	protected String name;
+	private Lock lock;
 
 	public CloudPath(CloudDirectory parent, String name) {
-		this.parent = parent;
-		this.name = name;
-		this.poolIdx = -1;
+		this(parent, name, -1);
 	}
 
 	public CloudPath(CloudDirectory parent, String name, int poolIdx) {
 		this.parent = parent;
 		this.name = name;
 		this.poolIdx = poolIdx;
+		this.lock = new ReentrantLock();
 	}
 
 	public int getPoolIdx() {
@@ -55,4 +59,9 @@ public abstract class CloudPath implements Poolable {
 	public boolean isInited() {
 		return isInited;
 	}
+
+	public Lock getLock() {
+		return lock;
+	}
+
 }
